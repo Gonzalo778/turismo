cde44469e514f9c848d3db56fe227717784e8551
Gonzalo778 <nicanor.179@gmail.com>
2022-01-26 11:12:32 -0400

    proyecto

---
+++ php/guardar.php
@@ -0,0 +1,34 @@
+<?php
+ //conectamos Con el servidor
+ $host ="aerial-summit-337923:us-east1:test";
+ $user ="test";
+ $pass ="test";
+ $db="sugerencias";
+
+ //funcion llamada conexion con (dominio,usuarios,contraseña,base_de_datos)
+ $con = mysqli_connect($host,$user,$pass,$db)or die("Problemas al Conectar");
+ mysqli_select_db($con,$db)or die("problemas al conectar con la base de datos");
+
+
+ //recuperar las variables
+ $nombre=$_POST['nombre'];
+ $correo=$_POST['correo'];
+ $mensaje=$_POST['mensaje'];
+ //hacemos la sentencia de sql
+ $sql="INSERT INTO datos VALUES('$nombre',
+                                 '$correo',
+                                 '$mensaje')";
+ //ejecutamos la sentencia de sql
+ $ejecutar=mysqli_query($con,$sql);
+ //verificamos la ejecucion
+ if(!$ejecutar){
+  echo"Hubo Algun Error";
+ }else{
+    echo'
+    <script>
+     alert("Mensaje enviado exitosamente");
+     window.location = "../formulario.html";
+     </script>
+     ';
+ }
+?>
